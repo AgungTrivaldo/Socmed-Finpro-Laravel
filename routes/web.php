8a4d936f7844407d2d3cbf9d284648f1556c36e8
AgungTrivaldo <yantosputra121@gmail.com>
2024-06-10 09:30:25 +0700

    add register

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/register', function () {
+    return view('register');
+});
+
 Route::get('/mainpage', function(){
     return view('forum');
 });
